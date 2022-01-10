9e63ea5662317cde088424a20766768425ba0c55
Fredrik <freddahf@outlook.com>
2022-01-10 13:14:21 +0100

    moving to pub-sub arch.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 	"sort"
+	"strings"
 	"syscall"
 	"time"
 
@@ -12,10 +13,12 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var BotId string
-var Events []calendar_util.CsvEvent
-var NotifyChannel string
-var StackOfEvents *Stack
+var (
+	BotId         string
+	Events        []calendar_util.CsvEvent
+	NotifyChannel string
+	StackOfEvents []*Stack
+)
 
 type StackNode struct {
 	Event calendar_util.CsvEvent
@@ -23,7 +26,11 @@ type StackNode struct {
 }
 
 type Stack struct {
-	Top *StackNode
+	Top     *StackNode
+	Channel string
+	Owner   string
+	Len     int
+	Active  bool
 }
 
 func (s *Stack) Push(event calendar_util.CsvEvent) {
@@ -32,18 +39,30 @@ func (s *Stack) Push(event calendar_util.CsvEvent) {
 		Next:  s.Top,
 	}
 	s.Top = node
+	s.Len += 1
 }
 
-func NewStack() *Stack {
-	return &Stack{}
+func NewStack(channel string, owner string) *Stack {
+	return &Stack{
+		Top:     nil,
+		Channel: channel,
+		Owner:   owner,
+		Len:     0,
+		Active:  true,
+	}
 }
 
-func ConvertToStack(events []calendar_util.CsvEvent) *Stack {
-	stack := new(Stack)
+func ConvertToStack(events []calendar_util.CsvEvent, channel string, owner string) *Stack {
+	stack := NewStack(channel, owner)
 	for i := len(events) - 1; i >= 0; i-- {
 		stack.Push(events[i])
+		println(stack.Len)
 	}
 	return stack
+}
+
+func (s Stack) String() string {
+	return fmt.Sprintf("%s, %s, %d events, Active %t", s.Owner, s.Channel, s.Len, s.Active)
 }
 
 func (s *Stack) Pop() *calendar_util.CsvEvent {
@@ -52,9 +71,11 @@ func (s *Stack) Pop() *calendar_util.CsvEvent {
 	}
 	node := s.Top
 	s.Top = s.Top.Next
+	s.Len -= 1
 	return &node.Event
 }
 
+// TODO: rewrite me!
 func WhenEvent(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == BotId {
 		return
@@ -79,14 +100,72 @@ func Help(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 }
 
-func NotifyEvent(s *discordgo.Session, r *discordgo.Ready) {
+func Join(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotId {
+		return
+	}
+
+	content := strings.Split(m.Content, " ")
+	urls := content[1:]
+
+	if content[0] != "!join" {
+		return
+	}
+
+	if len(content) < 2 {
+		s.ChannelMessageSend(m.ChannelID, "Usage: !join <urls> ")
+		return
+	}
+
+	csv, err := calendar_util.ReadCsvEvents(urls)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	sort.Slice(csv, func(i, j int) bool {
+		return csv[i].DtStart.Before(csv[j].DtStart.Time)
+	})
+	stack := ConvertToStack(csv, m.ChannelID, m.Author.Username)
+
+	StackOfEvents = append(StackOfEvents, stack)
+	go notify_events(&s, &stack)
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Thank you @%s for adding lecture notification bot. Service is now started, %d events will be notified", stack.Owner, stack.Len))
+}
+
+func Leave(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotId {
+		return
+	}
+
+	if m.Content == "!leave" {
+		for i := 0; i < len(StackOfEvents); i++ {
+			if StackOfEvents[i].Channel == m.ChannelID {
+				// TODO: remove the stack from the list
+				StackOfEvents[i].Active = false
+				StackOfEvents = append(StackOfEvents[:i], StackOfEvents[i+1:]...)
+				// s.ChannelMessageSend(m.ChannelID, "Service is now stopped")
+				return
+			}
+		}
+	}
+}
+
+func notify_events(s **discordgo.Session, stackofevents **Stack) {
 	var event *calendar_util.CsvEvent
+	session := *s
+	stack := *stackofevents
 	for {
+		fmt.Println(stack)
+		if !stack.Active {
+			session.ChannelMessageSend(stack.Channel, fmt.Sprintf("Service for Owner %s is now stopped", stack.Owner))
+			fmt.Println("Service is now stopped")
+			return
+		}
 		if event == nil {
-			event = StackOfEvents.Pop()
+			event = stack.Pop()
 			// if the stack is empty, we are done
 			if event == nil {
-				s.ChannelMessageSend(NotifyChannel, "No events left service is now stopped")
+				session.ChannelMessageSend(stack.Channel, "No events left service is now stopped")
 				fmt.Println("No events left service is now stopped")
 				return
 			}
@@ -94,19 +173,44 @@ func NotifyEvent(s *discordgo.Session, r *discordgo.Ready) {
 		}
 		timeUntilEvent := event.DtStart.Sub(time.Now())
 		if timeUntilEvent < time.Minute*15 {
-			s.ChannelMessageSend(NotifyChannel, event.String())
+			session.ChannelMessageSend(NotifyChannel, fmt.Sprintf("Hey %s, next event is:\n%s", stack.Owner, event.String()))
 			//i suppose the gc will take care of the memory
 			event = nil
 		}
 		//To lessen the load on the server, we sleep for a minute
-		time.Sleep(time.Minute)
+		time.Sleep(time.Second * 5)
 	}
 }
+
+//// TODO: rewrite me!
+//func NotifyEvent(s *discordgo.Session, r *discordgo.Ready) {
+//	var event *calendar_util.CsvEvent
+//	for {
+//		if event == nil {
+//			event = StackOfEvents.Pop()
+//			// if the stack is empty, we are done
+//			if event == nil {
+//				s.ChannelMessageSend(NotifyChannel, "No events left service is now stopped")
+//				fmt.Println("No events left service is now stopped")
+//				return
+//			}
+//			fmt.Printf("Event:\n%s\n", (*event).String())
+//		}
+//		timeUntilEvent := event.DtStart.Sub(time.Now())
+//		if timeUntilEvent < time.Minute*15 {
+//			s.ChannelMessageSend(NotifyChannel, event.String())
+//			//i suppose the gc will take care of the memory
+//			event = nil
+//		}
+//		//To lessen the load on the server, we sleep for a minute
+//		time.Sleep(time.Minute)
+//	}
+//}
 
 func main() {
 	// Create a new Discord session using the provided bot token.
 	token := "NTgwNDYyMjU2Mzk1OTExMTc3.XORDmg.4vplGl3G_bsEjmGukq0ppKBogyw"
-	NotifyChannel = "922805648251580416"
+	// NotifyChannel = "922805648251580416"
 	LecNotBot, err := discordgo.New("Bot " + token)
 	if err != nil {
 		panic(err)
@@ -119,21 +223,23 @@ func main() {
 
 	BotId = user.ID
 
-	urls := []string{"https://tp.uio.no/uit/timeplan/excel.php?type=course&sort=week&id[]=INF-3203%2C1&id[]=INF-3701%2C1", "https://tp.uio.no/ntnu/timeplan/excel.php?type=courseact&id%5B%5D=GEOG2023%C2%A4&id%5B%5D=KULMI2710%C2%A4&sem=22v&stop=1"}
-	csv, err := calendar_util.ReadCsvEvents(urls)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	Events = csv
-	sort.Slice(csv, func(i, j int) bool {
-		return csv[i].DtStart.Before(csv[j].DtStart.Time)
-	})
-	StackOfEvents = ConvertToStack(Events)
+	// urls := []string{"https://tp.uio.no/uit/timeplan/excel.php?type=course&sort=week&id[]=INF-3203%2C1&id[]=INF-3701%2C1", "https://tp.uio.no/ntnu/timeplan/excel.php?type=courseact&id%5B%5D=GEOG2023%C2%A4&id%5B%5D=KULMI2710%C2%A4&sem=22v&stop=1"}
+	// csv, err := calendar_util.ReadCsvEvents(urls)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// Events = csv
+	// sort.Slice(csv, func(i, j int) bool {
+	// 	return csv[i].DtStart.Before(csv[j].DtStart.Time)
+	// })
+	// StackOfEvents[0] = ConvertToStack(Events, NotifyChannel, "fredrik")
+	// LecNotBot.AddHandler(NotifyEvent)
 
 	LecNotBot.AddHandler(WhenEvent)
 	LecNotBot.AddHandler(Help)
-	LecNotBot.AddHandler(NotifyEvent)
+	LecNotBot.AddHandler(Join)
+	LecNotBot.AddHandler(Leave)
 
 	err = LecNotBot.Open()
 	if err != nil {
